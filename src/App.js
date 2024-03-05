df9b2a02466823258864d9dfac5ee3c805d773ce
LeonieNollevalle <leonie.nollevalle@gmail.com>
2024-03-05 16:18:03 +0100

    fini

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Bg() {
+    return (
+      <div className='w-screen h-screen  flex '>
+        
+        <div className='w-1/2 h-full p-5 '>
+
+        <div className='w-full flex justify-between pt-0 mt-0 '>
+          <p className='flex flex-col '>Maëlle Moisis</p>
+          <p className='flex flex-col '>Lucas Camsing</p>
+          <p className='flex flex-col '>Kilian Thivet</p>
+          <p className='flex flex-col '>Léonie Nollevalle</p>
+          </div>
+
+          <div className="w-full flex flex-col ">
+          <h1 className='uppercase font-bold flex flex-col pt-5 pb-0 mb-0 text-xl'>Paysage Typographique</h1>
+          <a href="https://www.figma.com/file/qHA8mD3CWG36KqBeiCmAX0/Paysage-Typographique?type=whiteboard&node-id=85%3A73&t=3HT06gPLK6meh1OF-1" className='flex items-center gap-2 bg-gray-200 rounded-lg p-3 my-2'> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"><path d="M12.25 2.834c-.46-.078-1.088-.084-2.22-.084c-1.917 0-3.28.002-4.312.14c-1.012.135-1.593.39-2.016.812c-.423.423-.677 1.003-.812 2.009c-.138 1.028-.14 2.382-.14 4.29v4c0 1.906.002 3.26.14 4.288c.135 1.006.389 1.586.812 2.01c.423.422 1.003.676 2.009.811c1.028.139 2.382.14 4.289.14h4c1.907 0 3.262-.002 4.29-.14c1.005-.135 1.585-.389 2.008-.812c.423-.423.677-1.003.812-2.009c.138-1.027.14-2.382.14-4.289v-.437c0-1.536-.01-2.264-.174-2.813h-3.13c-1.133 0-2.058 0-2.79-.098c-.763-.103-1.425-.325-1.954-.854c-.529-.529-.751-1.19-.854-1.955c-.098-.73-.098-1.656-.098-2.79zm1.5.776V5c0 1.2.002 2.024.085 2.643c.08.598.224.891.428 1.094c.203.204.496.348 1.094.428c.619.083 1.443.085 2.643.085h2.02a45.815 45.815 0 0 0-1.17-1.076l-3.959-3.563A37.2 37.2 0 0 0 13.75 3.61m-3.575-2.36c1.385 0 2.28 0 3.103.315c.823.316 1.485.912 2.51 1.835l.107.096l3.958 3.563l.125.112c1.184 1.065 1.95 1.754 2.361 2.678c.412.924.412 1.954.411 3.546v.661c0 1.838 0 3.294-.153 4.433c-.158 1.172-.49 2.121-1.238 2.87c-.749.748-1.698 1.08-2.87 1.238c-1.14.153-2.595.153-4.433.153H9.944c-1.838 0-3.294 0-4.433-.153c-1.172-.158-2.121-.49-2.87-1.238c-.748-.749-1.08-1.698-1.238-2.87c-.153-1.14-.153-2.595-.153-4.433V9.945c0-1.838 0-3.294.153-4.433c.158-1.172.49-2.121 1.238-2.87c.75-.749 1.701-1.08 2.878-1.238c1.144-.153 2.607-.153 4.455-.153h.056z"/><path d="M4.25 11.5A2.25 2.25 0 0 1 6.5 9.25h3A2.25 2.25 0 0 1 11.177 13a2.25 2.25 0 0 1-2.427 3.622v.878A2.25 2.25 0 1 1 4.823 16a2.242 2.242 0 0 1-.573-1.5c0-.576.217-1.102.573-1.5a2.242 2.242 0 0 1-.573-1.5m2.25 2.25a.75.75 0 1 0 0 1.5h.75v-1.5zm.75-1.5H6.5a.75.75 0 0 1 0-1.5h.75zm2.25 0a.75.75 0 0 0 0-1.5h-.75v1.5zm0 1.5a.75.75 0 1 0 0 1.5a.75.75 0 0 0 0-1.5m-2.25 3H6.5a.75.75 0 1 0 .75.75z"/></g></svg>
+          <p> Fichier de travail</p>
+          </a>
+          </div>
+          <p className="pt-5">Nous avons choisi la police de caractère Gosna <span className="font-bold">(fig.1)</span> comme base de développement pour l’exercice. La police de caractère Gosna a été créée par Dae Huen Lee qui est un étudiant en Design Graphique/Typographie à Estienne. Il a pensé sa police de caractère comme “des coups de pinceau qui s'écoulent de manière dynamique et gestuelle”. “Gosna s'inspire des improvisations solos de jazz et peut apporter à n'importe quel texte une qualité musicale vivante.” 
+          Une qualité musicale vivante qui nous a fait penser à Auriol<span className="font-bold">*</span> <span className="font-bold">(fig.2)</span>, une police de caractère iconique de l’Art Nouveau. Inspiré par la nature, ce mouvement artistique nous a donné envie de créer des paysages typographiques en utilisant la police de caractère Gosna, qui présente elle aussi des courbes naturelles. 
+          <br/>
+          <br/>
+          Notre volonté est de faire une nouvelle version de police organique liée à la nature.
+          La typographie Gosna nous a servi de support pour créer des lettres uniques, réalisées en utilisant un effet de miroir pour créer une dualité <span className="font-bold">(fig.3)</span>. 
+          <br/>
+          <br/>
+          Nous avons mis en scène ces nouveaux caractères à travers quatre éléments : sable, végétaux, eau et feu. Chaque mise en scène permet, grâce à la souris matérialisant l’effet du vent, de créer des expériences interactives sur chacun de ces éléments. La confrontation de chacun des éléments dans la page rappelle la dualité créée dans les caractères typographiques.
+          </p>
+          <p className="italic pt-5"><span className='font-bold text-lg'>*</span>(Auriol est une police de caractères à empattement triangulaire au pinceau, à effet pochoir, conçue par George Auriol en 1901 pour la fonderie G. Peignot & Fils).</p>
+        
+        <div className="w-full flex justify-between gap-3 pt-10 ">
+          <div className="w-1/4 flex flex-col gap-5">
+          <p><span className="font-bold">(Fig.1)</span> Gosna, Dae Huen Lee, 2021</p>
+          <img  src="https://cdn-djjbi.nitrocdn.com/PwrDEHmhGfvFuagMkOtDweGmuvdQKiJo/assets/images/optimized/rev-9c65dab/type-department.com/wp-content/uploads/2021/04/WebshopSlides_5.jpg" alt="gosna" />
+          </div>
+          
+          <div className="w-1/4">
+          <p><span className="font-bold">(Fig.2)</span> Auriol, George Auriol, 1901</p>
+          <img   src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Auriol_specimen.png" alt="gosna" />
+          </div>
+
+          <div className="w-1/4 flex flex-col gap-8">
+          <p><span className="font-bold">(Fig.3)</span> Gosna custom, LaTeam, 2024</p>
+          <svg className="w-full "  viewBox="0 0 366 351" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M366 287.664C366 307.118 343.869 310.933 270.99 310.933C212.23 310.933 179.785 315.897 179.785 240.379C179.785 214.823 183.602 180.879 190.857 134.352C194.295 112.612 196.962 92.0201 196.962 73.7151C196.962 41.6785 188.948 16.5018 163 7.73437C216.426 0.483474 230.924 10.4001 230.924 30.6126C230.924 39.38 227.486 52.7325 224.439 61.8909C242.363 28.6932 279.383 2 317.92 2C343.11 2 362.954 14.2033 362.954 34.4158C362.954 61.8791 326.325 83.2289 302.664 96.9606C285.488 106.877 234.741 141.201 243.133 149.968C252.676 160.264 306.481 115.266 354.94 146.153C311.827 121.746 258.022 177.81 234.361 181.234C225.589 182.384 218.714 175.512 222.531 161.022C232.832 121.355 333.567 90.468 333.567 46.2281C333.567 31.7381 320.978 22.9589 303.423 22.9589C279.762 22.9589 254.963 37.8279 239.316 57.2821C213.368 90.077 200.399 151.484 200.399 200.689C200.399 262.854 231.694 300.614 286.637 300.614C317.553 300.614 334.717 284.595 334.717 261.326C334.717 246.457 327.083 228.14 311.436 209.835C350.353 248.732 366 272.759 366 287.64V287.664Z" fill="black"/>
+          <path d="M54.5522 207.867C38.9049 226.172 31.2709 244.478 31.2709 259.36C31.2709 282.63 48.4474 298.649 79.3508 298.649C134.294 298.649 165.589 260.888 165.589 198.72C165.589 149.514 152.62 88.1159 126.672 55.3079C111.025 35.853 86.2261 20.9834 62.5655 20.9834C45.0097 20.9834 32.4207 29.7511 32.4207 44.2535C32.4207 88.4951 133.156 119.384 143.457 159.052C147.274 173.542 140.411 180.414 131.627 179.265C107.966 175.829 54.161 119.763 11.0479 144.182C59.5072 113.294 113.313 158.293 122.855 147.997C131.248 139.23 80.5006 104.905 63.3241 94.9879C39.6754 81.2439 3.03463 59.8814 3.03463 32.417C3.03463 12.2038 22.8783 0 48.068 0C86.6054 0 123.626 26.6943 141.561 59.8814C138.502 50.7226 135.076 37.3814 135.076 28.6018C135.076 8.38861 149.574 -1.52843 203 5.72274C177.052 14.4905 169.038 39.6682 169.038 71.706C169.038 90.0116 171.705 110.604 175.143 132.346C182.398 178.874 186.215 212.819 186.215 238.376C186.215 313.897 153.782 308.933 95.0099 308.933C22.1314 308.933 0 305.118 0 285.663C0 270.793 15.6473 246.765 54.564 207.855L54.5522 207.867Z" fill="#808080"/>
+          <path d="M101.624 322.548C100.822 322.756 100.34 323.574 100.548 324.376L103.932 337.445C104.139 338.247 104.958 338.729 105.76 338.521C106.562 338.313 107.044 337.495 106.836 336.693L103.828 325.076L115.445 322.068C116.247 321.861 116.729 321.042 116.521 320.24C116.313 319.438 115.495 318.956 114.693 319.164L101.624 322.548ZM273.706 322.727C219.712 356.412 158.713 355.643 102.761 322.707L101.239 325.293C158.087 358.757 220.288 359.588 275.294 325.273L273.706 322.727Z" fill="black"/>
+          </svg>
+          </div>
+         
+        </div>
+        
+
+
+        </div>
+
+        <div className='w-1/2 h-2/3 bg-red-600 flex flex-wrap h-screen overflow-y-scroll'>
+
+          <div className="w-full h-2/3 bg-green-100">
+          <iframe title="herbe" className='w-full h-full' src="https://editor.p5js.org/LeonieNollevalle/full/Uln4uEBjB"></iframe>
+          </div>
+          <div className="w-full h-2/3 bg-yellow-100">
+          <iframe title='sable' className='w-full h-full' src="https://editor.p5js.org/LeonieNollevalle/full/QcxIvNIbN"></iframe>
+          </div>
+          <div className="w-full h-2/3 bg-red-100">
+          <iframe title='feu' className='w-full h-full' src="https://editor.p5js.org/LeonieNollevalle/full/_t1niu3pR"></iframe>
+          </div>
+          <div className="w-full h-2/3 bg-blue-100">
+          <iframe title='bulle' className='w-full h-full' src="https://editor.p5js.org/LeonieNollevalle/full/QYJ3UDVbR"></iframe>
+          </div>
+        </div>
+     </div>
+    )
+      
 }
-
-export default App;
+export default Bg;
---
+++ src/componant/Paysage.jsx
@@ -0,0 +1,34 @@
+// import React from 'react'
+// import Sketch from "react-p5"
+
+// const Paysage = () => {
+//   var canvas;
+//   let x = 300;
+//   let y = 300;
+//   let dé;
+//   let pas = 20;
+//   let dir;
+// let font, fontsize = 600;
+
+// function preload() {
+//   font = loadFont('Rune-Regular.otf');
+// }
+//   const setup = (p5) => {
+//     canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+//     canvas.position(0, 0);
+//     p5.textFont(font);
+//     p5.textSize(fontsize);
+//     p5.textAlign(CENTER, CENTER);
+    
+//     let dir = - round(random(10,30));
+  
+//   }
+//   const draw = (p5) => {
+  
+//   };
+  
+  
+//   return <Sketch setup={setup} draw={draw} />
+// }
+
+// export default Paysage
